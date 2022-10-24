90f367db3be66c74b02649ae05460c2df70de7d0
riobedika <aquadanone321@gmail.com>
2022-10-24 20:09:58 +0700

    Pert 7

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,8 +37,16 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->post('/registrasi', 'RegistrasiController::registrasi');
+$routes->post('/registrasi', 'RegistrasiController::registrasi');
+$routes->post('/login', 'LoginController::login');
 
-
+$routes->group('produk', function ($routes) {
+    $routes->post('/', 'ProdukController::create');
+    $routes->get('/', 'ProdukController::list');
+    $routes->get('(:segment)', 'ProdukController::detail/$1');
+    $routes->put('(:segment)', 'ProdukController::ubah/$1');
+    $routes->delete('(:segment)', 'ProdukController::hapus/$1');
+   });
 /*
  * --------------------------------------------------------------------
  * Additional Routing
